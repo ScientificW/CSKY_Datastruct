78bd77c259df942288d63ea46e305a2a952b4787
ScientifcW <nbrichard@bupt.edu.cn>
2024-03-01 13:00:30 +0800

    分类

--- 栈/linkedStack.cpp
+++ 栈/linkedStack.cpp
@@ -4,7 +4,28 @@
 #include "iostream"
 using namespace std;
 
-typedef struct{
-    int* p;
+typedef struct SNode{
+    int data;
+    struct SNode *next;
+}*LinkedStack, SNode;
 
-}linkedStack;
+bool InitStack(LinkedStack &S){
+    S = (SNode*) malloc(sizeof (SNode));
+    if (S == NULL){
+        return false;
+    }
+    S->next = NULL;
+    return true;
+}
+
+bool Empty(LinkedStack S){
+    if(S->next == NULL){
+        return true;
+    }
+    return false;
+}
+
+SNode* GetTop(LinkedStack S, int i){
+    SNode *s = S->next;
+    return s;
+};
